19c1ea89f116a669bb0ae002e728ffd5c50a3dca
A.Sai Amar Chaitanya <saiamar110869@gmail.com>
2026-01-18 00:05:01 +0530

    mostwaterdsa
    mostwaterdsa

---
+++ mostwater.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+       int ans=0;
+       int lp=0;
+       int rp=height.size()-1;
+       while(lp>rp){
+        int w=rp-lp;
+        int h=min(height[lp],height[rp]);
+        ans=max(ans,w*h);
+        if(height[rp]>height[lp]){
+          lp++;
+        }
+        else{
+          rp--;
+
+        }
+
+       }return ans;
+
+    }
+};
