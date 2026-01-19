05ccc12b906f8d57b9c6d3ebb0c65a8c411a81bd
A.Sai Amar Chaitanya <saiamar110869@gmail.com>
2026-01-19 23:54:19 +0530

    update 121
    update 121

--- mostwater.cpp
+++ mostwater.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-       int ans=0;
+          int ans=0;
        int lp=0;
        int rp=height.size()-1;
-       while(lp>rp){
+       while(lp<rp){
         int w=rp-lp;
         int h=min(height[lp],height[rp]);
         ans=max(ans,w*h);
