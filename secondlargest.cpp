e45241ceb162d7f6bb2da1c786938db5cee6af78
A.Sai Amar Chaitanya <saiamar110869@gmail.com>
2026-01-20 23:51:01 +0530

    secondlargest
    second largest

---
+++ secondlargest.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+public:
+    int secondLargestElement(int arr[], int n){
+       int max=-1;
+       
+       int second=-1;
+       for(int i=0;i<n;i++){
+           if(arr[i]>max){
+                 second=max;
+               max=arr[i];
+             
+           }
+           if(arr[i]<max && arr[i]>second){
+               second=arr[i];
+               
+               
+           }
+       }
+        return second;
+        
+    }
+};
