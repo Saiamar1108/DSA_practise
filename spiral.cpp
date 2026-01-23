865c118210324c1432674b187a651de0517b1203
A.Sai Amar Chaitanya <saiamar110869@gmail.com>
2026-01-23 23:57:21 +0530

    update spiral
    update spiral

--- spiral.cpp
+++ spiral.cpp
@@ -1,20 +1,38 @@
-#include<bits/stdc++.h>
-using namespace std;
- 
-int main() {
-  int matrix[4][3]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
-  int rows=4;
-  int col=3;
-  int key=8;
-  for(int i=0;i<rows;i++){
-      for (int j=0;j<col;j++){
-         if(matrix[i][j]==key){
-             cout<<i<<endl;
-             cout<<j<<endl;
-             break;
-         }
-      }
-  }
-  
-  return 0;
-}
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int srow=0;
+        int scol=0;
+        int m=matrix.size();
+        int erow=m-1;
+        int ecol=matrix[0].size()-1;
+        while(srow<=erow && scol<=ecol){
+             for(int j=scol;j<=ecol;j++){
+           ans.push_back(matrix[srow][j]);
+        }
+        for(int i=srow-1;i<=erow;i++){
+           ans.push_back(matrix[i][ecol]);
+        }
+        for(int j=ecol-1;j>=scol;j++){
+            if(srow==erow){
+                break;
+            }
+           ans.push_back(matrix[erow][j]);
+        }
+        for(int i=erow-1;i<srow;i++){
+            if(scol==ecol){
+                break;
+            }
+           ans.push_back(matrix[i][scol]);
+        }
+        srow++;
+        scol++;
+        ecol--;
+        erow--;
+        }
+       
+
+        return ans;
+    }
+};
